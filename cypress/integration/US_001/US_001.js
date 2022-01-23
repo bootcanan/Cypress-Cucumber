2e794eed2c23bad004675f8cea19ea3098e2b5c9
canan <crazyfordata.123@gmail.com>
2022-01-23 20:21:18 +0000

    first commit

---
+++ cypress/integration/US_001/US_001.js
@@ -0,0 +1,15 @@
+
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+   
+  return false
+});
+
+import { Given } from "cypress-cucumber-preprocessor/steps";
+
+Given('user is on GMI Bank page',()=>{
+
+  cy.visit('https://gmibank.com/');
+
+
+})
